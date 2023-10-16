124eb974ac28aa63f74441cf68198fb6c4991a43
kapil619 <kapilbadokar321@gmail.com>
2023-10-16 14:21:55 +0530

    Spinner component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -12,6 +12,7 @@ import { createIssueSchema } from "@/app/validationSchema";
 
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -27,6 +28,8 @@ const NewIssuePage = () => {
   });
   const [error, setError] = useState("");
 
+  const [isSubmitting, setisSubmitting] = useState(false);
+
   return (
     <div className="max-w-xl">
       {error && (
@@ -38,9 +41,11 @@ const NewIssuePage = () => {
         className=" space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setisSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setisSubmitting(false);
             setError("An unexpected error occurred. Please try again.");
           }
         })}
@@ -57,7 +62,9 @@ const NewIssuePage = () => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
