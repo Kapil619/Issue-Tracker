78445eab9d6fcf0a060fd55cd83c73ee9125b859
kapil619 <kapilbadokar321@gmail.com>
2023-10-17 19:19:05 +0530

    Organised import statements in new file

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,6 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import { Heading, Flex, Card, Box } from "@radix-ui/themes";
-import React from "react";
-import ReactMarkdown from "react-markdown";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Box, Card, Flex } from "@radix-ui/themes";
+
+import { Skeleton } from "@/app/components";
 
 const LoadingIssueDetailPage = () => {
   return (
