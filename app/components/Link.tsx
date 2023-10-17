c0a17ea54f50fe6dea36149ce65166f1fef7ea5c
kapil619 <kapilbadokar321@gmail.com>
2023-10-17 18:41:42 +0530

    Link component (RadixUi+ Nextjs)

---
+++ app/components/Link.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NextLink from "next/link";
+import { Link as RadixLink } from "@radix-ui/themes";
+
+interface Props {
+  href: string;
+  children: string;
+}
+const Link = ({ href, children }: Props) => {
+  return (
+    <NextLink passHref legacyBehavior href={href}>
+      <RadixLink> {children} </RadixLink>
+    </NextLink>
+  );
+};
+
+export default Link;
