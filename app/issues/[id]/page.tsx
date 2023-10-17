d1982d751f0356bb35a5c85547934552e0686703
kapil619 <kapilbadokar321@gmail.com>
2023-10-17 12:00:50 +0530

    Showing issue details page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,25 @@
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const IsssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) return notFound();
+
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  );
+};
+
+export default IsssueDetailPage;
