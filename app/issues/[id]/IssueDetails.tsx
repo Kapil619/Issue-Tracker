8a2ea011aa18ad5773cacb684cc63eca30f9b6e5
kapil619 <kapilbadokar321@gmail.com>
2023-10-18 07:45:13 +0530

    Refactored Issue Details page (SRP)

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,22 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3" my={"2"}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose" mt={"4"}>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
