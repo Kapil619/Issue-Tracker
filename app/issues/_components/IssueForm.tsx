a230457b96dfcdd04fe939b921349d8ea89f52f2
kapil619 <kapilbadokar321@gmail.com>
2023-10-18 18:07:20 +0530

    Fixed Caching

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -40,6 +40,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setisSubmitting(false);
       setError("An unexpected error occurred. Please try again.");
