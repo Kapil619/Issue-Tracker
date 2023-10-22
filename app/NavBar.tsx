2d5a7d34f4b161000c808f7bb5249d5146772e48
kapil619 <kapilbadokar321@gmail.com>
2023-10-22 12:51:16 +0530

    Removed duplicate skeletons {huge refactor}

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   console.log(currentPath);
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
+    { label: "Issues", href: "/issues/list" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,7 +36,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setisSubmitting(true);
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setisSubmitting(false);
