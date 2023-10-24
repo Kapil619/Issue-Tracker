83880558751af98a0724110d93a31f65257c1002
kapil619 <kapilbadokar321@gmail.com>
2023-10-25 00:28:20 +0530

    Assigned issues to user | Changed dependency ver.

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,40 +1,44 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
-import { error } from "console";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest,
     { params }: { params: { id: string } }) {
 
-    const session = await getServerSession(authOptions);
-    if (!session) return NextResponse.json({}, { status: 401 });
+    // const session = await getServerSession(authOptions);
+    // if (!session) return NextResponse.json({}, { status: 401 });
 
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success)
         return NextResponse.json(validation.error.format(), {
             status: 400,
         });
 
+
+
     const { assignedToUserId, title, description } = body;
     if (assignedToUserId) {
-        const user = prisma.user.findUnique({
+        const user = await prisma.user.findUnique({
             where: { id: assignedToUserId }
-        })
+        });
         if (!user) return NextResponse.json({ error: 'Invalid user' },
             { status: 400 })
     }
+
+
 
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
     });
     if (!issue) {
         return NextResponse.json({ error: 'Invalid Issue' }, {
-            status: 400
+            status: 404
         })
     }
+
 
     const updatedIssue = await prisma.issue.update({
         where: { id: issue.id },
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -29,7 +29,7 @@ const IsssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction={"column"} gap={"4"}>
-            <AssigneeSelect />
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
