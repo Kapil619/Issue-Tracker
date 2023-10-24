47dda637752de552353203641881befc2df28201
kapil619 <kapilbadokar321@gmail.com>
2023-10-25 00:28:20 +0530

    Assigned issues to user | Changed dependency ver.

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -9,7 +9,7 @@ export const patchIssueSchema = z.object({
     title: z.string().min(1, 'Title is required').max(255).optional(),
     description: z.string().min(1, 'Description is required').max(65535).optional(),
 
-    assignedToUserId: z.string().min(1, 'AsignedToUserId is required').max(255).optional().nullable()
+    assignedToUserId: z.string().min(1, 'AsignedToUserId is required').max(255).optional().nullable(),
 });
 
 
