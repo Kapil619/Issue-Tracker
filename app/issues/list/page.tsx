391d140348be04280f379086b504fa6692a6dbd0
kapil619 <kapilbadokar321@gmail.com>
2023-10-25 18:57:29 +0530

    Implemented Sorting logic

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -20,10 +20,17 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
