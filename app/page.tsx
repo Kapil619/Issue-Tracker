3780fd5fff28442b0c24726f9e0b34f6dba9a86e
kapil619 <kapilbadokar321@gmail.com>
2023-10-26 23:54:05 +0530

    Built pagination layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
 }
