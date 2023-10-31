5d238eadf471396b0892af2ab210d902720802b3
kapil619 <kapilbadokar321@gmail.com>
2023-10-31 09:03:27 +0530

    Added Metadata to 3 files (Fixed metadata bug)

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -44,4 +44,14 @@ const IsssueDetailPage = async ({ params }: Props) => {
   );
 };
 
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  return {
+    title: issue?.title,
+    description: "Details of issue" + issue?.id,
+  };
+}
+
 export default IsssueDetailPage;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import LatestIssues from "./LatestIssues";
 import Pagination from "./components/Pagination";
 import IssueChart from "./IssueChart";
 import { Flex, Grid } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -22,3 +23,8 @@ export default async function Home() {
     </Grid>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "Summary of all your issues",
+};
